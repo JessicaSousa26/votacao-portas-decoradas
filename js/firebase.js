4fc1e43993e95a7a0bc3d64b76da837eaf9b2310
Jéssica Sousa <jhessymary26@gmail.com>
2025-11-07 01:51:36 -0300

    ajuste login google
    ajuste login google

--- js/firebase.js
+++ js/firebase.js
@@ -1,17 +1,34 @@
 // public/js/firebase.js
-// Configuração Firebase compatível com o projeto (v8+ compat syntax)
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-app.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, signOut, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-auth.js";
+import { getFirestore, collection, getDocs, getDoc, doc, addDoc, setDoc, query, where, orderBy, serverTimestamp, runTransaction } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-firestore.js";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "https://www.gstatic.com/firebasejs/12.5.0/firebase-storage.js";
 
+// 🔥 Configuração do seu projeto "votacao-portas-decoradas"
 const firebaseConfig = {
-  apiKey: "AIzaSyADR7Lcy4GoIUQsDUsveTNKJkm9hqNYtx8",
-  authDomain: "natal-votacao.firebaseapp.com",
-  projectId: "natal-votacao",
-  storageBucket: "natal-votacao.firebasestorage.app",
-  messagingSenderId: "210505457179",
-  appId: "1:210505457179:web:375c93056f57ed8982e2be",
-  measurementId: "G-P1LKGVHY10"
+  apiKey: "AIzaSyBudJ44QDyf7NQrQbfjsdJR-EWFbl8n56Y",
+  authDomain: "votacao-portas-decoradas.firebaseapp.com",
+  projectId: "votacao-portas-decoradas",
+  storageBucket: "votacao-portas-decoradas.firebasestorage.app",
+  messagingSenderId: "550340880775",
+  appId: "1:550340880775:web:febc94ae824853f70e8b22",
+  measurementId: "G-F3VK0JJH1Z"
 };
 
-// Inicializa o Firebase
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
-}
+// Inicializa Firebase
+const app = initializeApp(firebaseConfig);
+
+// Serviços do Firebase
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+const db = getFirestore(app);
+const storage = getStorage(app);
+
+// Exporta para uso em outros módulos
+export {
+  app, auth, provider, db, storage,
+  signInWithPopup, signOut, onAuthStateChanged,
+  collection, getDocs, getDoc, doc, addDoc, setDoc,
+  query, where, orderBy, serverTimestamp, runTransaction,
+  ref, uploadBytes, getDownloadURL
+};
